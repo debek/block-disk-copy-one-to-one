39bdf64402aa6b7609e2f3824e19aab9f9e2f3a8
Daniel Debny <debekgithub@gmail.com>
2022-12-18 21:47:15 +0100

    Improvements of README and code

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
-# block-disk-copy
+## block-disk-copy 1:1
 
-Backup disc 1:1 with master protection
+### Description:
 
-You have to change Your luks_org variable to Your disc Srial.
-You can change it using this command:
-smartctl -a /dev/sdb | grep Serial | awk '{print $3}'
+The script is making backup disc copy 1:1 with master disc protection.
+
+### HOW-TO:
+
+You have to change Your `luks_org` variable to your Serial disc.
+
+How to get disc serial: `smartctl -a /dev/sdb | grep Serial | awk '{print $3}'`
+
+`master_disc` - path to the master disc
+
+`backup_disc` - path to the backup
+
+`luks_org` - LUKS serial
--- luks_backup.sh
+++ luks_backup.sh
@@ -1,19 +1,19 @@
 #/bin/bash
-# Nalezy pamietac, ze oruginalny dysk musi byc na sdb a kopia na sdc.
-# Zmienna luks_org to nasz orygianly dysk
 
-luks_org=MS83N582310203L3F
-first_free_slot=/dev/sdb
+luks_org="MS83N582310203L3F"
+master_disc="/dev/sdb"
+backup_disc="/dev/sdc"
 luks_mounted=`sudo smartctl -a ${first_free_slot} | grep Serial | awk '{print $3}'`
-echo Oryginaly Serial dysku z danymi to $luks_org
-echo $first_free_slot to serial number $luks_mounted
 
-if [ "$luks_org" == "$luks_mounted" ]
+echo "Oryginal serial disc number is ${luks_org}"
+echo "${master_disc} serial number is ${luks_mounted}"
+
+if [ "${luks_org}" == "${luks_mounted}" ]
 then
-  echo "Kopiuje dane ..."
-  dd bs=4M if=/dev/sdb of=/dev/sdc status=progress oflag=sync
+  echo "Cpying data ..."
+  dd bs=4M if=${master_disc} of=${baskup_disc} status=progress oflag=sync
 else
-  echo "Nie mozna kopiowac poniewaz /dev/sdb nie jest dyskiem pierwotnym"
+  echo "Can't copy because ${master_disc} is not primary disc"
 fi
 
 exit 0
